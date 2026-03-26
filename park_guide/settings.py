36c2f0fab0ecc7bed7ccd872b5b1a6bd03923280
Ivan Liang Jin Ngu <74028102+MiyukiVigil@users.noreply.github.com>
2026-03-26 18:16:33 +0800

    Update settings.py

--- park_guide/settings.py
+++ park_guide/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/home/site/wwwroot/staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
