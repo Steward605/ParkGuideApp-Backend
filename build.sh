6b46438632ed1125dd5cca760a3cede314e5a2a1
MiyukiVigil <miyukivigil@gmail.com>
2026-05-07 10:25:10 +0800

    more fixes

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+pip uninstall -y opencv-python opencv-contrib-python || true
+pip install --no-cache-dir "opencv-python-headless>=4.8.0"
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
