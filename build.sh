fcf026bf06df00bf4ee3732a218e2636b82fa6da
MiyukiVigil <miyukivigil@gmail.com>
2026-05-07 13:12:48 +0800

    Revert "more fixes"
    This reverts commit 6b46438632ed1125dd5cca760a3cede314e5a2a1.

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-pip uninstall -y opencv-python opencv-contrib-python || true
-pip install --no-cache-dir "opencv-python-headless>=4.8.0"
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
